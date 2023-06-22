fb5bb785e1e35c04dcdba021c16c08e97942e942
Sergey <runix1981@gmail.com>
2023-06-22 16:27:17 +0300

    add comments to homework

--- HW_10/task_1.py
+++ HW_10/task_1.py
@@ -5,14 +5,14 @@ lst += ['human'] * 10
 random.shuffle(lst)
 num1 = 0
 num2 = 0
-data = pd.DataFrame(columns=['whoAmI', 'human', 'robot'])
+data = pd.DataFrame(columns=['whoAmI', 'human', 'robot'])    # Создаем пустой датафрейм с назнаниями колонок
 for i in range(len(lst)):
-    if lst[i] == 'human':
-        num1 = 1
-        num2 = 0
-        data.loc[len(data.index )] = [lst[i], num1, num2]
-    else:
-        num1 = 0
-        num2 = 1
-        data.loc[len(data.index)] = [lst[i], num1, num2]
-print(data)
+    if lst[i] == 'human':                                    # Если значения списка равно human, то 
+        num1 = 1                                             #  num1 = 1
+        num2 = 0                                             #  num2 = 0
+        data.loc[len(data.index )] = [lst[i], num1, num2]    # Добавляем значения в датафрейм при каждой итерации цикла
+    else:                                                    # Иначе
+        num1 = 0                                             #  num1 = 0
+        num2 = 1                                             #  num2 = 1
+        data.loc[len(data.index)] = [lst[i], num1, num2]     # Добавляем значения в датафрейм при каждой итерации цикла
+print(data)                                                  # Выводим на экран все строки датафрейма
